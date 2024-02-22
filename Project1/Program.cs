272c252d0ec638055abb612db16708bf0b9415b6
IGOR GUTIERREZ <ac.igor.gutierrez.dc@gmail.com>
2024-02-22 15:12:51 -0300

    Updates.

---
+++ Project1/Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+// int value = 0;
+// //Console.WriteLine("Hello, World!");
+
+// bool flag = true;
+// if (flag)
+// {
+//     Console.WriteLine($"Inside the code block: {value}");
+// }
+// value = 10;
+// Console.WriteLine($"Outside the code block: {value}");
+
+
+// bool flag = true;
+// if (flag)
+// {
+//     Console.WriteLine(flag);
+// }
+
+// bool flag = true;
+// if (flag) Console.WriteLine(flag);
+
+// string name = "steve";
+// if (name == "bob") Console.WriteLine("Found Bob");
+// else if (name == "steve") Console.WriteLine("Found Steve");
+// else Console.WriteLine("Found Chuck");
+
+
+// string name = "steve";
+
+// if (name == "bob")
+//     Console.WriteLine("Found Bob");
+// else if (name == "steve") 
+//     Console.WriteLine("Found Steve");
+// else
+//     Console.WriteLine("Found Chuck");
+
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+bool found = false;
+int total = 0;
+
+foreach (int number in numbers)
+{
+    total += number;
+
+    if (number == 42)
+       found = true;
+}
+
+if (found) 
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
+
+
